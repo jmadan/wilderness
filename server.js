86fd71388b67afeaba837dfd7360cf36549a6f0a
Jasdeep Madan <jasdeepm@gmail.com>
2019-11-07 22:26:58 +1100

    Using jest for testing

--- server.js
+++ server.js
@@ -1,29 +1,4 @@
-import 'dotenv/config'
-import express from 'express'
-import cors from 'cors'
-import morgan from 'morgan'
-import helmet from 'helmet'
-import bodyParser from 'body-parser'
-
-const app = express()
-app.use(cors())
-app.use(helmet())
-app.use(morgan('combined'))
-app.use(bodyParser.urlencoded({ extended: true }))
-// parse application/json
-app.use(bodyParser.json())
-
-app.use(function (req, res, next) {
-    //Enabling CORS 
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization");
-    next();
-});
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});  
+import app from './app'  
 
 const PORT = process.env.PORT
 
